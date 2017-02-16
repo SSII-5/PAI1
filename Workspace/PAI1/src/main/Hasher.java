a2fe8c98e731af6c83896f57b5053887287aa6d1
dalmaxor <amartingordillo@gmail.com>
2017-02-16 18:12:27 +0100

    El hasher por ahi anda

--- Workspace/PAI1/src/main/Hasher.java
+++ Workspace/PAI1/src/main/Hasher.java
@@ -1,5 +1,12 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64.Encoder;
+
 public class Hasher {
 	
 	
@@ -9,6 +16,7 @@ public class Hasher {
 	}
 	
 	private String type;
+	private static Encoder encoder;
 
 	
 	public String getType() {
@@ -21,14 +29,38 @@ public class Hasher {
 	}
 
 
-	public String hash(String type, String file){
-		String result;
+	public String hashFile(String file){
+		String result = null;
 		
-		
-		
-		
-		
-		
+	     
+		try {
+			InputStream fis =  new FileInputStream(file);
+
+		    byte[] buffer = new byte[8192];
+		    MessageDigest complete;
+		    byte[] hash;
+		    
+		    
+			complete = MessageDigest.getInstance(type);
+			
+			
+			int numRead;
+		     do {
+		      numRead = fis.read(buffer);
+		      if (numRead > 0) {
+		        complete.update(buffer, 0, numRead);
+		        }
+		      } while (numRead != -1);
+		     fis.close();
+		     hash = complete.digest();
+		     result = encoder.encodeToString(hash);
+		     return result;
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Este algoritmo no está soportado por el sistema, por favor, pruebe con otro distinto.");
+		} catch (IOException e) {
+			System.out.println("El sistema no ha podido acceder al archivo: "+ file);
+		}
 		return result;
+	     
 	}
 }
