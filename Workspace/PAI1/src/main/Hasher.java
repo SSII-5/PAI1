cb06e2fa40aed66d2b63000ebcc8d36c551fb806
dalmaxor <amartingordillo@gmail.com>
2017-02-16 16:50:07 +0100

    Try now

--- Workspace/PAI1/src/main/Hasher.java
+++ Workspace/PAI1/src/main/Hasher.java
@@ -1,5 +1,34 @@
 package main;
 
 public class Hasher {
+	
+	
+	public Hasher(String type){
+		super();
+		setType(type);
+	}
+	
+	private String type;
 
+	
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public String hash(String type, String file){
+		String result;
+		
+		
+		
+		
+		
+		
+		return result;
+	}
 }
