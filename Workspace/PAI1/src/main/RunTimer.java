1b773e5940f308290f3304b539328336b29c6ee8
Alberto <alberto0o095bm@gmail.com>
2017-02-20 19:43:13 +0100

    commito

--- Workspace/PAI1/src/main/RunTimer.java
+++ Workspace/PAI1/src/main/RunTimer.java
@@ -8,14 +8,7 @@ public class RunTimer extends TimerTask{
 	
 	public void run() {
 		
-		//Lee el fichero de configuracion
-		Reader.ReadConf();
-		
-		//cada 30 dias de ejecucion del programa, hace llamada al metodo generateInform (esa llamada se pone donde "task")
-		Date primeraVez = new Date(System.currentTimeMillis());
-		Timer informe = new Timer();
-		long tiempoEntreEjecuciones = 86400000 * Reader.ReadIntervalFromConf(); 
-		informe.schedule(task, primeraVez, tiempoEntreEjecuciones);
+		FileGenerator.hashedFiles();
 		
 	}
 
